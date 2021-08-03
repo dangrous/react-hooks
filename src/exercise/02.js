c6642fd96c66cfead0b86becb5ab67360d7e9d75
Daniel Gale-Rosen <dangalerosen@gmail.com>
2021-08-03 15:06:16 -0700

    Exercise 2 EC3

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,16 +1,23 @@
 // useEffect: persistent state
+
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') || initialName
+function useLocalStorageState(key, value = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || value
   )
 
   React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  },[name])
+    window.localStorage.setItem(key, state)
+  },[key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
